eda4a3316a4de19ece12cb87f8373622ad3d1b24
AntonHuggard <antonhuggard@gmail.com>
2022-05-03 13:04:27 +1200

    text corrections mostly

--- gallery.js
+++ gallery.js
@@ -1,15 +1,15 @@
 
-function gallery_control() {
+// function gallery_control() {
 
-    const images = ["img-capn", "img-cmdy", "img-bush"];
-    var n = 0;
-    const interval = setInterval(function() {
-        const photo = document.getElementById(images[n]);
-        photo.classList.remove("hidden");
-        photo.classList.add("slide-out");
-        // n++;
-        // n = n % 3;
-      }, 1000);
-}
+//     const images = ["img-capn", "img-cmdy", "img-bush"];
+//     var n = 0;
+//     const interval = setInterval(function() {
+//         const photo = document.getElementById(images[n]);
+//         photo.classList.remove("hidden");
+//         photo.classList.add("slide-out");
+//         // n++;
+//         // n = n % 3;
+//       }, 1000);
+// }
 
-gallery_control();
+// gallery_control();
