fa18c3c96a2339cbed21ac0df139695d7bf20043
AntonHuggard <antonhuggard@gmail.com>
2022-06-07 09:05:25 +1200

    quick fix: page loading

--- gallery.js
+++ gallery.js
@@ -1,15 +1,4 @@
 
-// function gallery_control() {
-
-//     const images = ["img-capn", "img-cmdy", "img-bush"];
-//     var n = 0;
-//     const interval = setInterval(function() {
-//         const photo = document.getElementById(images[n]);
-//         photo.classList.remove("hidden");
-//         photo.classList.add("slide-out");
-//         // n++;
-//         // n = n % 3;
-//       }, 1000);
-// }
-
-// gallery_control();
+window.addEventListener('load', function () {
+    document.getElementById("loading-container").style.display = "none";
+})
